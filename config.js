d1d69be39901da95d3cfdc76ad5fec3f3f4e0d16
Yarapwnz <yarabarabaluk@gmail.com>
2021-08-03 10:18:49 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+export default FirebaseKeys = {
+	apiKey: "AIzaSyDaPQQ5HFTKVtbKnJh7S3S2KqJj33emfn8",
+	authDomain: "react-chat-app-675d7.firebaseapp.com",
+	projectId: "react-chat-app-675d7",
+	storageBucket: "react-chat-app-675d7.appspot.com",
+	messagingSenderId: "1049766984787",
+	appId: "1:1049766984787:web:6b1b2fdb827615ee401aa6",
+};
